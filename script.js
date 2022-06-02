b255e81f9b1fe561b9a945dcd5df7fdc0b9d50b2
bernardofreire <b.bernardo9815@gmail.com>
2022-06-01 22:50:50 -0300

    adicionado nova função sidebar

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-var hamburguer = document.querySelector(".hamburguer")
+// Função para abri e fechar o sidebar ao clicar no hamburguer
+
+let hamburguer = document.querySelector(".hamburguer")
 
 
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu")
 })
+
+
+// funçao para fechar sidebar ao clicar em um link
+
+let closeLinks = document.querySelectorAll('.sidebar .menu ul a li')
+
+for(let closeLink of closeLinks){
+    closeLink.addEventListener('click', ()=>{
+        document.querySelector(".container").classList.toggle("show-menu")
+    })
+}
